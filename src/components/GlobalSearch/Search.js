21db94253cbc06772daf0853fe93e739c5b91c00
Alison Joseph <alisonejoseph@gmail.com>
2018-09-12 15:27:32 -0500

    add search

---
+++ src/components/GlobalSearch/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class GlobalSearch extends React.Component {
+ 
+  render() {
+    
+    return (
+      <div className="side-nav__search">
+       Search
+      </div>
+    );
+  }
+}
