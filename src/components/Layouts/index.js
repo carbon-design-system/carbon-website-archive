5d4eb80fdf1b4816db970f66f987377a86fa3b70
Alison Joseph <alisonejoseph@gmail.com>
2018-09-17 15:49:39 -0500

    fix for classname on reload issue on prod build

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -45,7 +45,7 @@ class Layout extends React.Component {
   
   render() {
     const { children } = this.props;
-    const classNames = classnames('container', {
+    const classNames = classnames({
       'container--expanded': !this.state.isOpen,
     });
 
@@ -71,7 +71,7 @@ class Layout extends React.Component {
             </Helmet>
             <SideNavToggleBtn onToggleBtnClick={this.onToggleBtnClick} isOpen={this.state.isOpen} />
             <SideNav isFinal={this.state.isFinal} isOpen={this.state.isOpen} location={this.props.location} />
-            <div className={classNames}>
+            <div className={`container ${classNames}`}>
               {children}
               <Footer isExpanded={this.state.isOpen} />
             </div>
