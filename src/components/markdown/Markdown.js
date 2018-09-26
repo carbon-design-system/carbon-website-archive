41ce3c88be5ffa2e1c1b65109d3acbcfe6d111f4
Alison Joseph <alisonejoseph@gmail.com>
2018-09-25 20:16:54 -0500

    Auto-link h2 headers

--- src/components/markdown/Markdown.js
+++ src/components/markdown/Markdown.js
@@ -38,12 +38,6 @@ export class FlexGroup extends React.Component {
   }
 }
 
-export class h2 extends React.Component {
-  render() {
-    return <h2 className="page-h2">{this.props.children}</h2>;
-  }
-}
-
 export class h3 extends React.Component {
   render() {
     return <h3 className="page-h3">{this.props.children}</h3>;
