8d3c2e6d01b6fbcfdc52ceaca4b2c0714792d22a
Alison Joseph <alisonejoseph@gmail.com>
2018-09-26 18:58:47 -0500

    Add border to video embed

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,9 +45,9 @@ module.exports = {
           {
             resolve: "gatsby-remark-embed-video",
             options: {
-              width: 800,
+              width: 606,
               ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
+              height: 333, // Optional: Overrides optional.ratio
               related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
               noIframeBorder: true //Optional: Disable insertion of <style> border: 0
             }
