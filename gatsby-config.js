238416bae15e8620c9d7e86fbe8411547d1da8c6
Alison Joseph <alisonejoseph@gmail.com>
2018-09-26 15:15:37 -0500

    Add video support for vimeo/youtube

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,16 @@ module.exports = {
         plugins: [
           `gatsby-remark-component`,
           {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+            }
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               // It's important to specify the maxWidth (in pixels) of
