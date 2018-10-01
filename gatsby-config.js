393090b26838cf34688c46432165f18366704cd4
Alison Joseph <alisonejoseph@gmail.com>
2018-10-01 16:32:13 -0500

    add links to images

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 590,
-              linkImagesToOriginal: false,
+              linkImagesToOriginal: true,
               backgroundColor: "transparent",
             },
           },
