0cab1d91edaf14e270dae70f92c52472c69d2299
Alison Joseph <alisonejoseph@gmail.com>
2018-10-09 12:07:25 -0500

    Halloween theme!

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -10,6 +10,7 @@ import favicon32 from '../../content/global/images/favicon-32.png';
 
 import '../../styles/index.scss';
 import '../../styles/experimental.scss';
+import '../../styles/halloween-components.scss';
 
 class Layout extends React.Component {
   static propTypes = {
