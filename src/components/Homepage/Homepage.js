35015669473f8043d926be38977413aaac9b0006
Alison Joseph <alisonejoseph@gmail.com>
2018-12-06 17:00:39 -0600

    update home image

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -12,7 +12,7 @@ export class HomepageHeader extends React.Component {
     const { GATSBY_CARBON_ENV } = process.env;
     const isInternal = GATSBY_CARBON_ENV == 'internal';
     // Banner image
-    const bannerImg = require('../../content/homepage/images/homepage-placeholder-img.png');
+    const bannerImg = require('../../content/homepage/images/homepage_img.jpg');
 
     const homeTitle = isInternal ? (
       <h3 className="homepage--header__title">
