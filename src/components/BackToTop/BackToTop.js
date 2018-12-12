5424b31656fe9521679dc990b99fb5cadf966def
Alison Joseph <alisonejoseph@gmail.com>
2018-12-12 15:20:24 -0600

    back to top scroll

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -3,11 +3,35 @@ import { Link } from 'gatsby';
 import { UpToTop20 } from '@carbon/icons-react';
 
 export default class BackToTop extends React.Component {
+  constructor() {
+    super();
+  
+    this.state = {
+        intervalId: 0
+    };
+  }
+  
+  scrollStep() {
+    if (window.pageYOffset === 0) {
+        clearInterval(this.state.intervalId);
+    }
+    window.scroll(0, window.pageYOffset - 50);
+  }
+  
+  scrollToTop() {
+    let intervalId = setInterval(this.scrollStep.bind(this), 4);
+    this.setState({ intervalId: intervalId });
+  }
+    
   render() {
     return (
-      <button className="back-to-top">
+      <button className="back-to-top" onClick={ () => { this.scrollToTop(); }}>
         <UpToTop20 aria-label="Back to Top" />
       </button>
     );
   }
 }
+
+
+
+
