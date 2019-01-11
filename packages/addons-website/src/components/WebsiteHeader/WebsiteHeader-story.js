2048e46d7cef8920f8ba5a514d7302367d7c2584
Alison Joseph <alisonejoseph@gmail.com>
2019-01-10 22:07:54 -0600

    add sidennav to website addon header story

--- packages/addons-website/src/components/WebsiteHeader/WebsiteHeader-story.js
+++ packages/addons-website/src/components/WebsiteHeader/WebsiteHeader-story.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import React from 'react';
+import { Fade16 } from '@carbon/icons-react';
 
 import {
   SkipToContent,
@@ -10,31 +11,52 @@ import {
   HeaderNavigation,
   HeaderMenuItem,
   HeaderMenu,
+  SideNav,
+  SideNavMenu,
+  SideNavLink,
+  SideNavMenuItem,
+  SideNavItems
 } from 'carbon-components-react/lib/components/UIShell';
 
 storiesOf('Website Header', module).add(
   'Website Header',
   () => (
-    <Header className="bx--header--website">
-       <SkipToContent />
-       <HeaderMenuButton
-          aria-label="Open menu"
-          onClick={action('Menu clicked')}
-        />
-        <HeaderName href="#" prefix="IBM">
-          Design
-        </HeaderName>
-        <HeaderNavigation aria-label="IBM [Platform]">
-          <HeaderMenu aria-label="Manage">
-            <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-            <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-          </HeaderMenu>
-          <HeaderMenuItem href="#" aria-current="page">Current</HeaderMenuItem>
-          <HeaderMenuItem href="#">Docs</HeaderMenuItem>
-          <HeaderMenuItem href="#">Support</HeaderMenuItem>
-        </HeaderNavigation>
-    </Header>
+    <>
+        <Header className="bx--header--website">
+        <SkipToContent />
+        <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={action('Menu clicked')}
+          />
+          <HeaderName href="#" prefix="IBM">
+            Design
+          </HeaderName>
+          <HeaderNavigation aria-label="IBM [Platform]">
+            <HeaderMenu aria-label="Manage">
+              <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+              <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+              <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+            </HeaderMenu>
+            <HeaderMenuItem href="#" aria-current="page">Current</HeaderMenuItem>
+            <HeaderMenuItem href="#">Docs</HeaderMenuItem>
+            <HeaderMenuItem href="#">Support</HeaderMenuItem>
+          </HeaderNavigation>
+      </Header>
+      <SideNav aria-label="Side navigation">
+      <SideNavItems>
+       <SideNavMenu title="Manage">
+          <SideNavMenuItem href="#" aria-current="page">Link 1</SideNavMenuItem>
+          <SideNavMenuItem href="#">Link 2</SideNavMenuItem>
+          <SideNavMenuItem href="#">Link 3</SideNavMenuItem>
+        </SideNavMenu>
+        
+        <SideNavLink href="#" aria-current="page">Current</SideNavLink>
+        <SideNavLink href="#">Docs</SideNavLink>
+        <SideNavLink href="#">Support</SideNavLink>
+
+      </SideNavItems>
+    </SideNav>
+  </>
   ),
   {
     info: {
