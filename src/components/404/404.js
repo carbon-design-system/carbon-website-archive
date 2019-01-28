f142bf6797a0ba4e08c3b35a523306d098581863
TJ Egan <tw15egan@gmail.com>
2019-01-28 14:54:39 -0600

    feat(404): start work on 404 page

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -5,20 +5,32 @@ const jackalope = require('../../content/404/images/jackalope.svg');
 class FourOhFour extends React.Component {
   render() {
     return (
-      <div className="page-container fourohfour-container">
-        <div className="fourohfour-large">
-          <img alt="404 jackalope" src={jackalope} />
-        </div>
-        <div className="fourohfour-caption">
-          <p className="fourohfour-title">
-            Congratulations! You found the illusive jackalope!
-          </p>
-          <p className="fourohfour-sub">
-            Unfortunately, we couldn't find the page you were looking for. Here
-            are some helpful links instead:
-            <Link to="/components">Components</Link>,
-            <Link to="/resources">Resources</Link>
-          </p>
+      <div className="page-content fourohfour">
+        <div className="ibm--grid">
+          <div className="ibm--row">
+            <div className="ibm--col-lg-12 ibm--offset-lg-4">
+              <h2>Something's wrong!</h2>
+              <p>Sorry, we cannot find the page you are looking for.</p>
+              <p>Maybe some of these most visited links will help you?</p>
+
+              <div className="anchor-links anchor-links--small">
+                <ul className="page-ul">
+                  <li>
+                    <Link to="/getting-started/about-carbon">Getting started</Link>
+                  </li>
+                  <li>
+                    <Link to="/components/overview">Component overview</Link>
+                  </li>
+                  <li>
+                    <Link to="/guidelines/iconography/library">Iconography</Link>
+                  </li>
+                  <li>
+                    <Link to="/resources">Resources</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
