1a2bcb77b49b9555ea251a25c153758f86eebec5
TJ Egan <tw15egan@gmail.com>
2019-01-28 16:21:17 -0600

    feat(404): more work on 404 page

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -6,29 +6,36 @@ class FourOhFour extends React.Component {
   render() {
     return (
       <div className="page-content fourohfour">
+        <div className="fourohfour-repeat">
+          <p className="fourohfour-repeat__text">04040404040</p>
+          <p className="fourohfour-repeat__text">40404040404</p>
+          <p className="fourohfour-repeat__text">04040404040</p>
+          <p className="fourohfour-repeat__text">40404040404</p>
+        </div>
         <div className="ibm--grid">
           <div className="ibm--row">
-            <div className="ibm--col-lg-12 ibm--offset-lg-4">
-              <h2>Something's wrong!</h2>
-              <p>Sorry, we cannot find the page you are looking for.</p>
-              <p>Maybe some of these most visited links will help you?</p>
+            <div className="ibm--col-lg-12 ibm--offset-lg-4 fourohfour-body">
+              <div className="fourohfour-body">
+                <h2 className="fourohfour-body__heading">Something's wrong!</h2>
+                <p className="fourohfour-body__text">Sorry, we cannot find the page you are looking for.</p>
+                <p className="fourohfour-body__text">Maybe some of these most visited links will help you?</p>
+                <div className="anchor-links anchor-links--small fourohfour-body__anchor-links">
+                  <ul className="page-ul">
+                    <li>
+                      <Link to="/getting-started/about-carbon" >Getting started</Link>
+                    </li>
+                    <li>
+                      <Link to="/components/overview">Component overview</Link>
+                    </li>
+                    <li>
+                      <Link to="/guidelines/iconography/library">Iconography</Link>
+                    </li>
+                    <li>
+                      <Link to="/resources">Resources</Link>
+                    </li>
+                  </ul>
+                </div></div>
 
-              <div className="anchor-links anchor-links--small">
-                <ul className="page-ul">
-                  <li>
-                    <Link to="/getting-started/about-carbon">Getting started</Link>
-                  </li>
-                  <li>
-                    <Link to="/components/overview">Component overview</Link>
-                  </li>
-                  <li>
-                    <Link to="/guidelines/iconography/library">Iconography</Link>
-                  </li>
-                  <li>
-                    <Link to="/resources">Resources</Link>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
         </div>
