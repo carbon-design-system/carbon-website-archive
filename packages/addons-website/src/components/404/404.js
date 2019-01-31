b93777800c9f24e0e28d362eae3c255502018e63
TJ Egan <tw15egan@gmail.com>
2019-01-31 15:46:14 -0600

    fix(404): add to addons, fix sidebar color

--- packages/addons-website/src/components/404/404.js
+++ packages/addons-website/src/components/404/404.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
-const jackalope = require('../../content/404/images/jackalope.svg');
 
 class FourOhFour extends React.Component {
   render() {
+    const { children } = this.props;
+
     if (typeof document !== 'undefined') {
       document.body.style.background = '#282828';
     }
@@ -16,22 +16,7 @@ class FourOhFour extends React.Component {
                 <h2 className="fourohfour-body__heading">Something's wrong!</h2>
                 <p className="fourohfour-body__text">Sorry, we cannot find the page you are looking for.</p>
                 <p className="fourohfour-body__text">Maybe some of these most visited links will help you?</p>
-                <div className="anchor-links anchor-links--small fourohfour-body__anchor-links">
-                  <ul className="page-ul">
-                    <li>
-                      <Link to="/getting-started/about-carbon" >Getting started</Link>
-                    </li>
-                    <li>
-                      <Link to="/components/overview">Component overview</Link>
-                    </li>
-                    <li>
-                      <Link to="/guidelines/iconography/library">Iconography</Link>
-                    </li>
-                    <li>
-                      <Link to="/resources">Resources</Link>
-                    </li>
-                  </ul>
-                </div>
+                {this.props.children}
                 <h5 className="fourohfour-body__404">404</h5>
               </div>
             </div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import Layout from '../components/Layouts';
-import FourOhFour from '../components/404';
+import { FourOhFour } from '@carbon/addons-website';
+import { Link } from 'gatsby';
 
 import '../polyfills';
 
 class NotFoundPage extends React.Component {
+  componentDidMount() {
+    let sideNav = document.querySelector('.bx--side-nav');
+
+    if (sideNav.classList.contains('bx--side-nav--website--light')) {
+      sideNav.classList.remove('bx--side-nav--website--light');
+    }
+  }
+
   render() {
     return (
       <Layout>
-        <FourOhFour />
+        <FourOhFour>
+          <div className="anchor-links anchor-links--small fourohfour-body__anchor-links">
+            <ul className="page-ul">
+              <li>
+                <Link to="/getting-started/about-carbon" >Getting started</Link>
+              </li>
+              <li>
+                <Link to="/components/overview">Component overview</Link>
+              </li>
+              <li>
+                <Link to="/guidelines/iconography/library">Iconography</Link>
+              </li>
+              <li>
+                <Link to="/resources">Resources</Link>
+              </li>
+            </ul>
+          </div>
+        </FourOhFour>
       </Layout>
     );
   }
