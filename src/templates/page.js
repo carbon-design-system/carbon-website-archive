a5454604e432d4ed958e6753c8fef53651e8f5dd
Alison Joseph <alisonejoseph@gmail.com>
2019-02-19 07:26:00 -0600

    update template

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,99 +1,26 @@
 import '../polyfills';
 import React from 'react';
-import rehypeReact from 'rehype-react';
+import { graphql } from 'gatsby';
+import MDXRenderer from 'gatsby-mdx/mdx-renderer';
+
 import Layout from '../components/Layouts';
 import { FourOhFour } from '@carbon/addons-website';
 
 import classnames from 'classnames';
 
 // Components
+import FeatureTile from '../components/FeatureTile';
 import PageHeader from '../components/PageHeader';
 import PageTabs from '../components/PageTabs';
-import Snippet from '../components/CodeSnippet';
-import PageTable from '../components/PageTable';
-import ClickTile from '../components/ClickableTile';
-import FeatureTile from '../components/FeatureTile';
-import DoDontExample from '../components/DoDontExample';
-import ColorBlock from '../components/ColorBlock';
-import ColorCard from '../components/ColorCard';
-import IconLibrary from '../components/IconLibrary';
-import TypeScaleTable from '../components/TypeScaleTable';
-import TypeWeight from '../components/TypeWeight';
-import TypeSpec from '../components/TypeSpec';
-import ComponentCode from '../components/ComponentCode';
-import ComponentDocs from '../components/ComponentDocs';
-import ComponentStatus from '../components/ComponentStatus';
-import ComponentReact from '../components/ComponentReact';
-import Glossary from '../components/Glossary';
-import ComponentOverview from '../components/ComponentOverview';
 import NextPrevious from '../components/NextPrevious';
-import GridWrapper from '../components/GridWrapper';
 import BackToTop from '../components/BackToTop';
 import {
   HomepageFooter,
   HomepageHeader,
 } from '../components/Homepage/Homepage';
-import { Tab } from 'carbon-components-react';
-import WebsiteTabs from '../components/WebsiteTabs';
-import ColorTokenTable from '../components/ColorTokenTable';
-import SimpleColumns from '../components/SimpleColumns';
-import ImageComponent from '../components/ImageComponent';
-
-// Custom Markdown
-import {
-  p,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  ul,
-  ol,
-  PageIcon,
-  AnchorLinks,
-} from '../components/markdown/Markdown';
-
-const renderAst = new rehypeReact({
-  createElement: React.createElement,
-  components: {
-    p: p,
-    h1: h1,
-    h2: h2,
-    h3: h3,
-    h4: h4,
-    h5: h5,
-    ul: ul,
-    ol: ol,
-    pre: Snippet,
-    table: PageTable,
-    'grid-wrapper': GridWrapper,
-    icon: PageIcon,
-    'clickable-tile': ClickTile,
-    'feature-tile': FeatureTile,
-    'do-dont-example': DoDontExample,
-    'color-block': ColorBlock,
-    'color-card': ColorCard,
-    'icon-library': IconLibrary,
-    'type-scale-table': TypeScaleTable,
-    'type-weight': TypeWeight,
-    'type-spec': TypeSpec,
-    component: ComponentCode,
-    'component-react': ComponentReact,
-    'component-docs': ComponentDocs,
-    'component-status': ComponentStatus,
-    glossary: Glossary,
-    'component-overview': ComponentOverview,
-    'anchor-links': AnchorLinks,
-    tab: Tab,
-    'website-tabs': WebsiteTabs,
-    'color-token-table': ColorTokenTable,
-    'simple-columns': SimpleColumns,
-    'image-component': ImageComponent,
-  },
-}).Compiler;
 
 export default ({ data }) => {
-  const post = data.markdownRemark;
+  const post = data.mdx;
   let currentPage = post.fields.currentPage;
   let slug = post.fields.slug;
   let tabs = post.frontmatter.tabs;
@@ -119,7 +46,7 @@ export default ({ data }) => {
         <div className="container--homepage">
           <HomepageHeader />
           <main className="page-content ibm--grid" id="maincontent">
-            {renderAst(post.htmlAst)}
+            <MDXRenderer>{post.code.body}</MDXRenderer>
           </main>
           <HomepageFooter />
         </div>
@@ -141,7 +68,7 @@ export default ({ data }) => {
           )}
         </PageHeader>
         <main className="page-content ibm--grid" id="maincontent">
-          {renderAst(post.htmlAst)}
+          <MDXRenderer>{post.code.body}</MDXRenderer>
         </main>
         <NextPrevious
           slug={slug}
@@ -155,9 +82,11 @@ export default ({ data }) => {
 };
 
 export const query = graphql`
-  query BlogPostQuery($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      htmlAst
+  query($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      code {
+        body
+      }
       fields {
         slug
         currentPage
