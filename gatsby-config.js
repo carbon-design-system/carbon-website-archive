aca028f417a1b99afe3b82f87485135e51a32400
Alison Joseph <alisonejoseph@gmail.com>
2019-02-19 08:10:52 -0600

    add global scope

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,17 +15,28 @@ module.exports = {
       resolve: `gatsby-mdx`,
       options: {
         root: __dirname,
-        //extensions: ['.mdx', '.md'],
         defaultLayouts: {
           default: require.resolve('./src/components/Layouts/index.js'),
         },
-        /*    globalScope: `
-          import { Button } from 'carbon-components-react';
-          export default { Button };
-          import FeatureTile from '${__dirname}/src/components/FeatureTile';
-          import ClickableTile from '${__dirname}/src/components/ClickableTile';
-          import GridWrapper from '${__dirname}/src/components/GridWrapper';
-        `, */
+        globalScope: `
+        import AnchorLinks from '${__dirname}/src/components/AnchorLinks';
+        import ClickableTile from '${__dirname}/src/components/ClickableTile';
+        import FeatureTile from '${__dirname}/src/components/FeatureTile';
+        import GridWrapper from '${__dirname}/src/components/GridWrapper';
+        import DoDontExample from '${__dirname}/src/components/DoDontExample';
+        import ComponentCode from '${__dirname}/src/components/ComponentCode';
+        import ComponentDocs from '${__dirname}/src/components/ComponentDocs';
+
+        export default {
+          AnchorLinks,
+          ClickableTile,
+          FeatureTile,
+          GridWrapper,
+          DoDontExample,
+          ComponentDocs,
+          ComponentCode
+        };
+      `,
         gatsbyRemarkPlugins: [
           /* { resolve: 'gatsby-remark-unwrap-images' },
           {
