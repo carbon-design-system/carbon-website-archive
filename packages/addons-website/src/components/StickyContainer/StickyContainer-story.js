b31dec03010e2bb1d4f5e6a68602ec0bd1327a06
ryan moylan <ryan.moylan@adrenaline-havas.com>
2019-03-06 16:52:12 -0500

    addressed PR comments

--- packages/addons-website/src/components/StickyContainer/StickyContainer-story.js
+++ packages/addons-website/src/components/StickyContainer/StickyContainer-story.js
@@ -80,4 +80,45 @@ storiesOf('Sticky Container', module)
         text: 'Sticky Container',
       },
     }
+  )
+  .add(
+    'Sticky Container with navBar and banner',
+    () => (
+      <div>
+        <div
+          style={{
+            width: '100%',
+            height: '2.5rem',
+            backgroundColor: '#666',
+            position: 'sticky',
+            top: '0',
+            color: '#FFF',
+          }}>
+          this is banner
+        </div>
+        <div
+          style={{
+            width: '100%',
+            height: '3rem',
+            backgroundColor: '#333',
+            position: 'sticky',
+            top: '2.5rem',
+            color: '#FFF',
+          }}>
+          this is navBar
+        </div>
+        <div style={{ width: '100%', height: '5rem' }} />
+        <StickyContainer navBar={true} banner={true}>
+          <h1>This is a sticky title header</h1>
+        </StickyContainer>
+        <div
+          style={{ width: '100%', height: '3000px', position: 'relative' }}
+        />
+      </div>
+    ),
+    {
+      info: {
+        text: 'Sticky Container',
+      },
+    }
   );
