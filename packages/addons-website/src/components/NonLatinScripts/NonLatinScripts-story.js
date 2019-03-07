92a59713a8e9fe72abaa92f6b40602abf9c175e3
ryan moylan <ryan.moylan@adrenaline-havas.com>
2019-03-07 16:35:56 -0500

    updated storybook to handle individual scripts, or standalone

--- packages/addons-website/src/components/NonLatinScripts/NonLatinScripts-story.js
+++ packages/addons-website/src/components/NonLatinScripts/NonLatinScripts-story.js
@@ -1,12 +1,64 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import NonLatinScripts from '.';
+import NonLatinScript from './NonLatinScript';
 
 storiesOf('Non Latin Scripts', module).add(
   'Non Latin Scripts',
   () => (
     <div style={{width: '100%', backgroundColor: '#F3F3F3' }}>
       <NonLatinScripts />
+    </div>
+  ),
+  {
+    info: {
+      text: 'Non Latin Scripts',
+    },
+  }
+)
+.add(
+  'Individual Non Latin Scripts',
+  () => (
+    <div style={{width: '100%', backgroundColor: '#F3F3F3' }}>
+      <NonLatinScript
+        customStyle="--non-latin-type-example-cyrillic"
+        typefaceName="IBM Plex Mono Cyrillic"
+        className="ibm-type-mono"
+        value="инженер"
+      />
+      <NonLatinScript
+        customStyle="--non-latin-type-example-cyrillic"
+        typefaceName="IBM Plex Sans Cyrillic"
+        value="инженер"
+      />
+      <NonLatinScript
+        customStyle="--non-latin-type-example-cyrillic"
+        typefaceName="IBM Plex Serif Cyrillic"
+        className="ibm-type-serif"
+        value="инженер"
+        />
+      <NonLatinScript
+        customStyle="--non-latin-type-example-devanagari"
+        typefaceName="IBM Plex Devanagari"
+        className="ibm-type-devanagari"
+        value="संज्ञानात्मक"
+      />
+      <NonLatinScript
+        typefaceName="IBM Plex Sans Greek"
+        value="νομίζω"
+        />
+      <NonLatinScript
+        typefaceName="IBM Plex Sans Hebrew"
+        className="ibm-type-hebrew"
+        rtl
+        value="לחשׁוֹב"
+        />
+      <NonLatinScript
+        customStyle="--non-latin-type-example-thai"
+        typefaceName="IBM Plex Thai (non-looped)"
+        className="ibm-type-thai"
+        value="องค์ความรู้"
+        />
     </div>
   ),
   {
