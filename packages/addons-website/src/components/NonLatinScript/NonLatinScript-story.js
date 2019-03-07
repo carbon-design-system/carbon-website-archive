41831eb17f7e8c1b66b413b9c823ec1c571bc04c
ryan moylan <ryan.moylan@adrenaline-havas.com>
2019-03-07 14:42:06 -0500

    cleanup, small tweaks

--- packages/addons-website/src/components/NonLatinScript/NonLatinScript-story.js
+++ packages/addons-website/src/components/NonLatinScript/NonLatinScript-story.js
@@ -5,7 +5,9 @@ import NonLatinScript from '.';
 storiesOf('Non Latin Script', module).add(
   'Non Latin Script',
   () => (
-    <NonLatinScript />
+    <div style={{width: '100%', backgroundColor: '#F3F3F3' }}>
+      <NonLatinScript />
+    </div>
   ),
   {
     info: {
