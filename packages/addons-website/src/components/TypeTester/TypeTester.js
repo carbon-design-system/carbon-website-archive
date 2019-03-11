e55a1027d63f9e4c9c2311854ca028ca9ef5c956
basepath added <Maggie.Ryder@havas.com>
2019-03-11 14:28:33 -0400

    Type Tester widget: first draft
    Todo: add correct font classes from carbon
    Todo: fix dropdown mouse out styling (this may be resolved with updated Dropdown component)
    Todo: textarea dynamic height settings

---
+++ packages/addons-website/src/components/TypeTester/TypeTester.js
@@ -0,0 +1,338 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Dropdown from 'carbon-components-react/lib/components/DropdownV2'
+import TextArea from 'carbon-components-react/lib/components/TextArea'
+// import DropdownItem from 'carbon-components-react/lib/components/DropdownItem'
+import InputRange from '@carbon/addons-website/src/components/InputRange'
+
+import { settings } from 'carbon-components';
+const prefix = settings.prefix;
+
+
+const HEBREW = 'hebrew'
+
+// const ibmTypeCondensedCSS = css`
+//   /* .ibm-type-condensed does not exist in @ibmduo/type */
+//   .ibm-type-condensed,
+//   .ibm-type-condensed * {
+//     font-family: 'IBM Plex Sans Condensed' !important;
+//   }
+
+//   @media (max-width: ${breakpointsREM.sm}) {
+//     // margin-left: -1rem;
+//     // margin-right: -1rem;
+//   }
+// `
+
+const languageSample = [
+  {
+    language: 'latin',
+    content: 'Heavy boxes perform quick waltzes and jigs.',
+  },
+  {
+    language: 'cyrillic',
+    content: 'Эх, чужак, общий съём цен шляп (юфть)—вдрызг!',
+  },
+  {
+    language: 'greek',
+    content: 'διαφυλάξτε γενικά τη ζωή σας από βαθειά ψυχικά τραύματα.',
+  },
+  {
+    language: 'hebrew',
+    content: 'דג סקרן שט בים מאוכזב ולפתע מצא חברה.',
+  },
+  {
+    language: 'thai',
+    content:
+      'นายสังฆภัณฑ์ เฮงพิทักษ์ฝั่ง ผู้เฒ่าซึ่งมีอาชีพเป็นฅนขายฃวด ถูกตำรวจปฏิบัติการจับฟ้องศาล ฐานลักนาฬิกาคุณหญิงฉัตรชฎา ฌานสมาธิ',
+  },
+  {
+    language: 'devanagari',
+    content:
+      'ऋषियों को सताने वाले दुष्ट राक्षसों के राजा रावण का सर्वनाश करने वाले विष्णुवतार भगवान श्रीराम, अयोध्या के महाराज दशरथ के बड़े सपुत्र थे।',
+  },
+]
+
+const commonFontWeights = [
+  { value: 100, label: 'Thin' },
+  { value: 200, label: 'ExtraLight' },
+  { value: 300, label: 'Light' },
+  { value: 400, label: 'Regular' },
+  { value: 450, label: 'Text' },
+  { value: 500, label: 'Medium' },
+  { value: 600, label: 'SemiBold' },
+  { value: 700, label: 'Bold' },
+]
+
+const languageDropdownContent = [
+  {
+    label: 'IBM Plex Sans Hebrew', 
+    language: 'hebrew', 
+    className: 'ibm-type-hebrew',
+    weights: commonFontWeights, 
+  },
+  {
+    label: 'IBM Plex Sans',
+    language: 'latin',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Sans Italic',
+    language: 'latin',
+    className: 'ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Condensed',
+    language: 'latin',
+    className: 'ibm-type-condensed',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Condensed Italic',
+    language: 'latin',
+    className: 'ibm-type-condensed ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Mono',
+    language: 'latin',
+    className: 'ibm-type-mono',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Mono Italic',
+    language: 'latin',
+    className: 'ibm-type-mono ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Serif',
+    language: 'latin',
+    className: 'ibm-type-serif',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Serif Italic',
+    language: 'latin',
+    className: 'ibm-type-serif ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Sans Cyrillic',
+    language: 'cyrillic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Sans Cyrillic Italic',
+    language: 'cyrillic',
+    className: 'ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Mono Cyrillic',
+    language: 'cyrillic',
+    className: 'ibm-type-mono',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Mono Cyrillic Italic',
+    language: 'cyrillic',
+    className: 'ibm-type-mono ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Serif Cyrillic',
+    language: 'cyrillic',
+    className: 'ibm-type-serif',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Serif Cyrillic Italic',
+    language: 'cyrillic',
+    className: 'ibm-type-serif ibm-type-italic',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Sans Greek',
+    language: 'greek',
+    weights: commonFontWeights,
+  },
+  {
+    label: 'IBM Plex Sans Greek Italic',
+    language: 'greek',
+    className: 'ibm-type-italic',
+    weights: commonFontWeights,
+  },
+
+  {
+    label: 'IBM Plex Sans Thai',
+    language: 'thai', 
+    className: 'ibm-type-thai',
+    weights: commonFontWeights, 
+  },
+
+  {
+    label: 'IBM Plex Sans Devanagari',
+    language: 'devanagari',
+    className: 'ibm-type-devanagari',
+    weights: commonFontWeights,
+  }, 
+].map(variant => {
+  variant.value = variant.label.toLowerCase().replace(/ /g, '-')
+  return variant
+})
+
+class TypeTester extends Component {
+  state = {
+    typeSizeMultiplier: 470,
+    variant: 'ibm-plex-sans-hebrew',
+    lastVariant: 'ibm-plex-sans-hebrew',
+    fontWeight: 400,
+    text: languageSample.find(el => el.language === 'hebrew').content,
+    openDropdown: null,
+  }
+
+  onFontWeightDropdownChange = ({ selectedItem }) => {
+    this.setState({
+      fontWeight: selectedItem.value,
+    })
+  }
+
+  onLanguageDropdownChange = ({ selectedItem }) => {
+    const { label, value } = selectedItem
+    this.setState({
+      lastVariant: this.state.variant,
+    })
+    setTimeout(() => {
+      this.setState({
+        variant: value,
+        text: this.getDefaultTextForVariant(value, this.state.lastVariant),
+        fontWeight: this.getCurrentOrDefaultFontWeightForLanguage(
+          this.getLanguageForVariant(value),
+          this.state.fontWeight,
+        ),
+      })
+    }, 100);
+  }
+
+  onChangeOpenDropdown = dropdown => {
+    this.setState({
+      openDropdown: dropdown === this.state.openDropdown ? null : dropdown,
+    })
+  }
+
+  getCurrentOrDefaultFontWeightForLanguage = (language, currentWeight) => {
+    return currentWeight
+  }
+
+  getClassNameForVariant = variant => {
+    return (
+      languageDropdownContent.find(item => item.value === variant).className || null
+    )
+  }
+
+  getDefaultTextForVariant = (variant, lastVariant) => {
+      if (this.getLanguageForVariant(variant) != this.getLanguageForVariant(lastVariant)) {
+      return languageSample.find(item => item.language === this.getLanguageForVariant(variant)).content 
+    }
+  }
+
+  getLanguageForVariant = variant => {
+    return languageDropdownContent.find(item => item.value === variant).language
+  }
+
+  getWeightsForLanguage = () => {
+    const language = this.getLanguageForVariant(this.state.variant)
+    const languageConfig = languageDropdownContent.find(i => i.language === language);
+    return languageConfig.weights;
+  }
+
+  getLanguageForWeight = weight => {
+    const weightObj = commonFontWeights.find(w => w.value === weight)
+    return weightObj.label
+  }
+
+  isRtl = () => {
+    return this.getLanguageForVariant(this.state.variant) === HEBREW
+  }
+
+  render() {
+    const lineHeight = Math.max(
+      (
+        1.375 +
+        0.025 +
+        this.state.typeSizeMultiplier / 100 -
+        (this.state.typeSizeMultiplier / 100) * 1.025
+      ).toFixed(4)
+    )
+    const textClasses = classnames(
+      `${prefix}--text-area`,
+      this.getClassNameForVariant(this.state.variant)
+    );
+    
+    return (
+      <div className={`${prefix}--type-tester-container`}>
+        
+            <div className={`${prefix}--type-tester-menu`}>
+              <div className="dropdown_wrapper">
+                <Dropdown
+                  items={languageDropdownContent}
+                  label={this.state.variant}
+                  onChange={this.onLanguageDropdownChange}
+                  selected={this.state.variant}
+                  onChangeOpen={this.onChangeOpenDropdown.bind(
+                    null,
+                    'language-dropdown'
+                  )}
+                  open={this.state.openDropdown === 'language-dropdown'}
+                />
+                <Dropdown
+                  items={this.getWeightsForLanguage()}
+                  label={this.getLanguageForWeight(this.state.fontWeight)}
+                  onChange={this.onFontWeightDropdownChange}
+                  selected={this.state.fontWeight}
+                  onChangeOpen={this.onChangeOpenDropdown.bind(
+                    null,
+                    'weight-dropdown'
+                  )}
+                  open={this.state.openDropdown === 'weight-dropdown'}
+                />
+              </div>
+              <div className={`${prefix}--input-range-wrapper`}>
+                <InputRange
+                  className={`${prefix}--input-range`}
+                  min={100}
+                  max={1600}
+                  value={this.state.typeSizeMultiplier}
+                  onChange={e => {
+                    this.setState({ typeSizeMultiplier: Number(e.target.value) })
+                  }}
+                />
+              </div>
+            </div>
+            <div className={`${prefix}--type-tester-sample`}>
+              <textarea 
+                className={textClasses}
+                style={{
+                  fontSize: this.state.typeSizeMultiplier / 100 + 'em',
+                  lineHeight: lineHeight,
+                  fontWeight: this.state.fontWeight,
+                  direction: this.isRtl() ? 'rtl' : 'ltr'
+                }}
+                value={this.state.text}
+                onChange={e => this.setState({ text: e.target.value })}
+              />
+            </div>
+          
+      </div>
+    )
+  }
+}
+
+TypeTester.propTypes = {
+
+}
+
+export default TypeTester
