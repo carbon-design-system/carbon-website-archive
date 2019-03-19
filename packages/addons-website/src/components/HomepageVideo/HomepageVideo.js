aa9d613496d0878db3f8b77b3240cde93257ea04
ryan moylan <ryan.moylan@adrenaline-havas.com>
2019-03-19 17:29:35 -0400

    implement looping for homepageVideo

--- packages/addons-website/src/components/HomepageVideo/HomepageVideo.js
+++ packages/addons-website/src/components/HomepageVideo/HomepageVideo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { settings } from 'carbon-components';
+import { breakpoints } from "@carbon/layout/es";
 const { prefix } = settings;
 
 import { ArrowRight20 } from "@carbon/icons-react/es";
@@ -10,7 +11,34 @@ import PlayPauseButton from '@carbon/addons-website/src/components/PlayPauseButt
 
 class HomepageVideo extends Component {
   state = {
-    playing: true
+    playing: true,
+    loop: true
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    const {loop } = this.state
+    
+    if (window.matchMedia(`(max-width: ${breakpoints.md.width})`).matches) {
+      if( loop ) {
+        this.setState({
+          loop: false
+        })
+      } 
+    } else {
+      if ( !loop ) {
+        this.setState({
+          loop: true
+        })
+      }
+    }
   }
 
   onPlayPauseClick = () => {
@@ -26,15 +54,16 @@ class HomepageVideo extends Component {
   }
 
   render() {
+    const {loop, playing} = this.state
+
     return (
       <div className={`${prefix}--homepage-video-container ibm--offset-lg-4`}>
         <div className={`${prefix}--homepage-video-wrapper`}>
           <VideoInternal
-            // loop={viewportWidth >= Number(breakpointsREM.md.replace('rem', ''))*16}
-            loop={true}
+            loop={loop}
             poster="images/hero-video-poster.jpg"
             src="videos/hero-video.mp4"
-            playing={this.state.playing}
+            playing={playing}
             onVideoComplete={this.onVideoComplete}
           />
           <div className={`${prefix}--homepage-video-overlay`} />
@@ -52,7 +81,7 @@ class HomepageVideo extends Component {
               </div>
             </Link>
           </div>
-           <PlayPauseButton onClick={this.onPlayPauseClick} playing={this.state.playing} />
+           <PlayPauseButton onClick={this.onPlayPauseClick} playing={playing} />
         </div>
       </div>
     )
