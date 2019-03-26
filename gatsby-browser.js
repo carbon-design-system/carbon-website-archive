5c4402c4c8a1e2469aa474b3d188f5716a53eab7
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-03-25 22:20:10 -0500

    fix: move to CDM

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,26 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.onRouteUpdate = ({ location }) => {
-    const SmoothScroll = require('smooth-scroll');
-    const scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 400,
-        durationMin: 250,
-        durationMax: 700,
-        easing: 'easeInOutCubic',
-        offset: 87, // height of both header bars
-        topOnEmptyHash: false,
-        clip: true
-    });
-
-    if (location.hash) {
-        const hashElement = document.querySelector(location.hash);
-        if (hashElement.offsetTop) {
-            window.scrollTo(0, hashElement.offsetTop - 16);
-        } else {
-            // IE fallback
-            scroll.animateScroll(hashElement);
-        }
-    }
-};
