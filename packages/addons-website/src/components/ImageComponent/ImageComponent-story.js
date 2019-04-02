997a92a4745847fdc65b5ad91590067803488666
basepath added <Maggie.Ryder@havas.com>
2019-04-02 13:10:19 -0400

    Mr carousel #1415
    addressing comments

--- packages/addons-website/src/components/ImageComponent/ImageComponent-story.js
+++ packages/addons-website/src/components/ImageComponent/ImageComponent-story.js
@@ -13,25 +13,25 @@ storiesOf('Image Component', module).add(
     },
   }
 )
-// .add(
-//   'Image Component with Zoom no Title',
-//   () => <ImageComponent caption="This is a caption" fixed="default" zoom="true">
-//             <img src="https://loremflickr.com/400/300" alt="alt text" />
-//         </ImageComponent>,
-//   {
-//     info: {
-//       text: 'Image Component with Zoom no Title',
-//     },
-//   }
-// )
-// .add(
-//   'Image Component no Zoom',
-//   () => <ImageComponent caption="This is a caption" fixed="default">
-//             <img src="https://loremflickr.com/400/300" alt="alt text" title="This is the title" />
-//         </ImageComponent>,
-//   {
-//     info: {
-//       text: 'Image Component no Zoom',
-//     },
-//   }
-// );
+.add(
+  'Image Component with Zoom no Title',
+  () => <ImageComponent caption="This is a caption" fixed="default" zoom="true">
+            <img src="https://loremflickr.com/400/300" alt="alt text" />
+        </ImageComponent>,
+  {
+    info: {
+      text: 'Image Component with Zoom no Title',
+    },
+  }
+)
+.add(
+  'Image Component no Zoom',
+  () => <ImageComponent caption="This is a caption" fixed="default">
+            <img src="https://loremflickr.com/400/300" alt="alt text" title="This is the title" />
+        </ImageComponent>,
+  {
+    info: {
+      text: 'Image Component no Zoom',
+    },
+  }
+);
