3a48ceef066706ef75d1e571010c7693ffa27051
Alison Joseph <alisonejoseph@gmail.com>
2019-04-15 14:49:09 -0500

    chore: add settings import

--- packages/addons-website/src/components/AnchorLinks/AnchorLinks.js
+++ packages/addons-website/src/components/AnchorLinks/AnchorLinks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
+import { settings } from 'carbon-components';
 const { prefix } = settings;
 
 export default class AnchorLinks extends React.Component {
