bfadd733ed3d48415c7878da6d3e480547002e56
Lee Chase <lee.chase@uk.ibm.com>
2019-08-15 13:57:49 +0100

    Revert "fix: 404 flash before redirect"
    This reverts commit 9215f87b6b4e90f8a3473d363b20b36866bba5e4.

--- packages/addons-website/src/components/404/404.js
+++ packages/addons-website/src/components/404/404.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { settings } from 'carbon-components';
 const { prefix } = settings;
-const browser = typeof window !== 'undefined' && window;
 
 class FourOhFour extends React.Component {
   render() {
@@ -9,30 +8,28 @@ class FourOhFour extends React.Component {
       document.body.style.background = '#282828';
     }
     return (
-      browser && (
-        <div className="page-content fourohfour">
-          <div className={`${prefix}--grid`}>
-            <div className={`${prefix}--row`}>
-              <div
-                className={`${prefix}--col-lg-12 ${prefix}--offset-lg-4 fourohfour-body`}>
-                <div className="fourohfour-body">
-                  <h2 className="fourohfour-body__heading">
-                    Something's wrong...
-                  </h2>
-                  <p className="fourohfour-body__text">
-                    Sorry, we cannot find the page you are looking for.
-                  </p>
-                  <p className="fourohfour-body__text">
-                    Maybe some of these most visited links will help you?
-                  </p>
-                  {this.props.children}
-                  <h5 className="fourohfour-body__404">404</h5>
-                </div>
+      <div className="page-content fourohfour">
+        <div className={`${prefix}--grid`}>
+          <div className={`${prefix}--row`}>
+            <div
+              className={`${prefix}--col-lg-12 ${prefix}--offset-lg-4 fourohfour-body`}>
+              <div className="fourohfour-body">
+                <h2 className="fourohfour-body__heading">
+                  Something's wrong...
+                </h2>
+                <p className="fourohfour-body__text">
+                  Sorry, we cannot find the page you are looking for.
+                </p>
+                <p className="fourohfour-body__text">
+                  Maybe some of these most visited links will help you?
+                </p>
+                {this.props.children}
+                <h5 className="fourohfour-body__404">404</h5>
               </div>
             </div>
           </div>
         </div>
-      )
+      </div>
     );
   }
 }
